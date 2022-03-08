87b4f2d2c5109aa5b71c2ef655731b104f41df4d
Greg <lupasco.grigori@gmail.com>
2022-03-08 21:15:25 +0200

    difficulty levels added

---
+++ app/src/main/java/utils/Constants.kt
@@ -0,0 +1,19 @@
+package utils
+
+import com.example.memorygame.R
+
+val DEFAULT_ICONS = listOf(
+    R.drawable.ic_android,
+    R.drawable.ic_cloud,
+    R.drawable.ic_drink,
+    R.drawable.ic_face,
+    R.drawable.ic_fire,
+    R.drawable.ic_flower,
+    R.drawable.ic_grass,
+    R.drawable.ic_headphones,
+    R.drawable.ic_heart,
+    R.drawable.ic_mountain,
+    R.drawable.ic_pizza,
+    R.drawable.ic_sun,
+
+)
